84dba321b8c0ca1e0471d48fd751f45e741cbe81
slalonde <sebastien.lalonde@ipsen.com>
2017-10-11 15:34:22 +0200

    add tags

--- createRess.tf
+++ createRess.tf
@@ -3,4 +3,9 @@
 resource "azurerm_resource_group" "testSL-TF001" {
     name = "testSL-TF001"
     location = "westeurope"
+    tags {
+        environment = "test"
+        project = "terraformAutoTraining"
+        domain = "IOS-IPO"
+    }  
 }
