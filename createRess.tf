e32809840bc4f02a3eb6e93cb3b9da266f494ddf
slalonde <sebastien.lalonde@ipsen.com>
2017-10-11 15:50:54 +0200

    add storage

--- createRess.tf
+++ createRess.tf
@@ -9,3 +9,21 @@ resource "azurerm_resource_group" "testSL-TF001" {
         domain = "IOS-IPO"
     }  
 }
+
+
+# Compte de stockage
+# Storage Account
+resource "azurerm_storage_account" "testSL-TF001-StorAccount" {
+  name                = "storageaccountpackertestsl001"
+  resource_group_name = "${azurerm_resource_group.testSL-TF001.name}"
+  location            = "${azurerm_resource_group.testSL-TF001.location}"
+  account_type        = "Standard_LRS"
+}
+
+# Container dans le compte de stockage
+resource "azurerm_storage_container" "testSL-TF001-Container" {
+   name = "VM-images"
+   resource_group_name = "${azurerm_resource_group.testSL-TF001.name}"
+   storage_account_name = "${azurerm_storage_account.testSL-TF001-StorAccount.name}"
+   container_access_type = "private"
+}
